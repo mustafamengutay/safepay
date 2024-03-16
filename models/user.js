2e852c28e50a1406227f66b7e9d88a337e54758f
Mustafa <mustafamengutayy@gmail.com>
2024-03-16 22:02:02 +0200

    feat: add salary fields

--- models/user.js
+++ models/user.js
@@ -22,6 +22,28 @@ const userSchema = new mongoose.Schema({
         required: true,
         default: 0,
     },
+    netSalary: {
+        socialInsurance: {
+            type: Number,
+            required: true,
+            default: 0,
+        },
+        generalHealthSystem: {
+            type: Number,
+            required: true,
+            default: 0,
+        },
+        totalTax: {
+            type: Number,
+            required: true,
+            default: 0,
+        },
+        monthlyNetSalary: {
+            type: Number,
+            required: true,
+            default: 0,
+        },
+    },
 }, { timestamps: true, });
 
 module.exports = mongoose.model('User', userSchema);
