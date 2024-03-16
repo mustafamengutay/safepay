9cbfa8bb1632c0c46b44355106b172f06265d673
Mustafa <mustafamengutayy@gmail.com>
2024-03-16 19:06:01 +0200

    feat: add mongodb connection & attach admin routes

--- app.js
+++ app.js
@@ -1,19 +1,36 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv').config();
+
+const adminRoutes = require('./routes/admin');
 
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
 
+app.use('/admin', adminRoutes);
+
 app.use((error, req, res, next) => {
     console.log(error);
     const statusCode = error.statusCode || 500;
     const message = error.message;
+    const errors = error.array;
     res.status(statusCode).json({
         message,
+        errors,
     });
 });
 
-app.listen(3000);
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log('Server is running!');
+        app.listen(3000);
+    })
+    .catch(error => {
+        console.log('Server Connection Error!');
+        console.log(error);
+    });
+
