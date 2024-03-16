6c50790d0822b76ddb10eadf9679a8153d022deb
Mustafa <mustafamengutayy@gmail.com>
2024-03-16 19:03:17 +0200

    feat: add admin model

---
+++ models/admin.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const adminSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+}, { timestamps: true, });
+
+module.exports = mongoose.model('Admin', adminSchema);
