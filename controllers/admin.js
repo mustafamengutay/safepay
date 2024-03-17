a6f2b457b29036746dc7b73d7c237434e4fc002f
Mustafa <mustafamengutayy@gmail.com>
2024-03-17 12:57:28 +0200

    feat: add create admin functionality

--- controllers/admin.js
+++ controllers/admin.js
@@ -2,6 +2,43 @@ const { validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
 
 const User = require('../models/user');
+const Admin = require('../models/admin');
+
+/**
+ * @description     Create an admin
+ * @route           POST /admin/create-admin
+ * @access          Private
+ */
+const postCreateAdmin = async (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        const error = new Error('Validation Error!');
+        error.statusCode = 422;
+        error.array = errors.array();
+        return next(error);
+    }
+
+    try {
+        const email = req.body.email;
+        const password = await bcrypt.hash(req.body.password, 12);
+
+        const newAdmin = new Admin({
+            email,
+            password,
+        });
+        const admin = await newAdmin.save();
+
+        res.status(201).json({
+            message: 'Admin created successfully!',
+            admin,
+        });
+    } catch (error) {
+        if (!error.statusCode) {
+            error.statusCode = 500;
+        }
+        next(error);
+    }
+};
 
 /**
  * @description     Create a user
@@ -43,5 +80,6 @@ const postCreateUser = async (req, res, next) => {
 };
 
 module.exports = {
+    postCreateAdmin,
     postCreateUser,
 };
