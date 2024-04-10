efb91f62ea839cda08b75a97c6010e1cf39cf586
Mustafa <mustafamengutayy@gmail.com>
2024-04-10 20:54:15 +0300

    feat: add load balancer

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const dotenv = require('dotenv').config();
 const helmet = require('helmet');
 const rateLimit = require('express-rate-limit');
 const io = require('./sockets/socket');
+const numCPUs = require('os').availableParallelism();
+const cluster = require('cluster');
 
 const errorHandler = require('./middlewares/error');
 
@@ -14,34 +16,50 @@ const userRoutes = require('./routes/user');
 const staffRoutes = require('./routes/staff');
 const adminRoutes = require('./routes/admin');
 
-const app = express();
+if (cluster.isPrimary) {
+    console.log(`Cluster Manager ${process.pid} is running`);
 
-const limiter = rateLimit({
-    windowMs: 10 * 60 * 1000,
-    limit: 100,
-});
+    for (let i = 0; i < numCPUs; i++) {
+        cluster.fork();
+    }
 
-app.use(cors());
-app.use(helmet());
-app.use(limiter);
-app.use(bodyParser.json());
-
-app.use('/admin', adminRoutes);
-app.use('/staff', staffRoutes);
-app.use('/user', userRoutes);
-app.use(authRoutes);
-
-app.use(errorHandler.customErrorHandler);
-
-mongoose.connect(process.env.MONGO_URI)
-    .then(() => {
-        const server = app.listen(3000);
-        console.log('Server is running!');
-        io.initializeSocket(server).on('connection', () => {
-            console.log('Client connected!');
-        });
-    })
-    .catch(error => {
-        console.log('Server Connection Error!');
-        console.log(error);
+    cluster.on('exit', (worker) => {
+        console.log(`worker ${worker.process.pid} died`);
+        cluster.fork();
     });
+} else {
+    const app = express();
+
+    const limiter = rateLimit({
+        windowMs: 10 * 60 * 1000,
+        limit: 100,
+        message: 'Too many requests!',
+        legacyHeaders: false,
+        standardHeaders: 'draft-7',
+    });
+
+    app.use(cors());
+    app.use(helmet());
+    app.use(limiter);
+    app.use(bodyParser.json());
+
+    app.use('/admin', adminRoutes);
+    app.use('/staff', staffRoutes);
+    app.use('/user', userRoutes);
+    app.use(authRoutes);
+
+    app.use(errorHandler.customErrorHandler);
+
+    mongoose.connect(process.env.MONGO_URI)
+        .then(() => {
+            const server = app.listen(3000);
+            console.log('Server is running!');
+            io.initializeSocket(server).on('connection', () => {
+                console.log('Client connected!');
+            });
+        })
+        .catch(error => {
+            console.log('Server Connection Error!');
+            console.log(error);
+        });
+}
