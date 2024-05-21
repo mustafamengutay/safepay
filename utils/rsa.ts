c91795ea81035729e4d2d2aea8716e16ae050fa7
Mustafa <mustafamengutayy@gmail.com>
2024-05-21 17:58:54 +0300

    feat: add RSA public/private keys
    - keys are used when sending DES-encrypted invoices

---
+++ utils/rsa.ts
@@ -0,0 +1,37 @@
+import { generateKeyPairSync } from 'crypto';
+
+export const { publicKey, privateKey } = generateKeyPairSync('rsa', {
+  modulusLength: 2048,
+  publicKeyEncoding: {
+    type: 'pkcs1',
+    format: 'pem',
+  },
+  privateKeyEncoding: {
+    type: 'pkcs1',
+    format: 'pem',
+  },
+});
+
+/**
+ *
+ * @param data Any data which will be hashed by XOR.
+ * @returns A buffer which is a hash value.
+ */
+export const xorHash = (data: any) => {
+  const buffer = Buffer.from(data);
+  let hash = 0;
+
+  for (let i = 0; i < buffer.length; i++) {
+    hash ^= buffer[i];
+  }
+
+  // Limit hash to 48 bits
+  return Buffer.from([
+    (hash >> 40) & 0xff,
+    (hash >> 32) & 0xff,
+    (hash >> 24) & 0xff,
+    (hash >> 16) & 0xff,
+    (hash >> 8) & 0xff,
+    hash & 0xff,
+  ]);
+};
